a99146f140a01e6a290a76101309e86b708bce65
Artiverma <vermaarti930@gmail.com>
2025-12-04 20:40:30 +0530

    Delete mini_fullStack directory

--- mini_fullStack/JavaScript/metaData.js
+++
@@ -1,3 +0,0 @@
-//https://mini-full-stack-app-d4b11-default-rtdb.asia-southeast1.firebasedatabase.app/
-
-export const url="https://mini-full-stack-app-d4b11-default-rtdb.asia-southeast1.firebasedatabase.app/";
