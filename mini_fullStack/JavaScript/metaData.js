faae6eb4a16f14d656fe4f9fbdc1a29e9333280d
Artiverma <vermaarti930@gmail.com>
2025-12-04 20:38:02 +0530

    Add files via upload

---
+++ mini_fullStack/JavaScript/metaData.js
@@ -0,0 +1,3 @@
+//https://mini-full-stack-app-d4b11-default-rtdb.asia-southeast1.firebasedatabase.app/
+
+export const url="https://mini-full-stack-app-d4b11-default-rtdb.asia-southeast1.firebasedatabase.app/";
